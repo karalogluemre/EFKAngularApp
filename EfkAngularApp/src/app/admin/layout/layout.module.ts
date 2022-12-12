0edd5e40d2f8e3a5c6834998a5298ce0321c22cd
Emre KARALOĞLU <emrekaraloglu@gmail.com>
2022-12-12 16:31:06 +0300

    Angular Material Design Sidebar

--- EfkAngularApp/src/app/admin/layout/layout.module.ts
+++ EfkAngularApp/src/app/admin/layout/layout.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { ComponentsModule } from './components/components.module';
 import { RouterModule } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   imports: [
     CommonModule,
     ComponentsModule,
-    RouterModule
+    RouterModule,
+    MatSidenavModule
   ],
   declarations: [LayoutComponent],
   exports:[
